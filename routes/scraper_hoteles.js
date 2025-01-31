d44f6212a0a02e169e76ba4826e014fbf4429271
Vanessa Gonzalez <vanessagpro5@gmail.com>
2025-01-30 21:34:05 -0600

    add url

--- routes/scraper_hoteles.js
+++ routes/scraper_hoteles.js
@@ -3,6 +3,7 @@ const azabache_func = require('./functions/azabache')
 const checkhotel_func = require('./functions/check_hotel')
 const bedsonline_func = require('./functions/bedsonline')
 const regio_func = require('./functions/regio')
+const olr_func = require('./functions/olr')
 
 const urls = [
     //{
@@ -10,31 +11,32 @@ const urls = [
     //    url: 'https://vtoreservaciones.com/Hotel/Hotel.aspx',
     //    funct: vto_func
     //},
-    {
-        operadora: 'Regio',
-        url: 'https://b2b.regio.travel/home',
-        funct: regio_func
-    },
-    //https://vtoreservaciones.com/Hotel/Hotel.aspx
-    {
-        operadora: 'Check Hotel',
-        url: 'https://checkhotel.mx/',
-        funct: checkhotel_func
-    },
-    {
-        operadora: 'Bedsonline',
-        url: 'https://app.bedsonline.com/main',
-        funct: bedsonline_func
-    },
-    {
-        operadora: 'Azabache',
-        url: 'https://azabache.paquetedinamico.com/login.xhtml?microsite=azabache&keepurl=true&url=%2Fhome',
-        funct: azabache_func
-    }
-    //{   
-    //    operadora: 'OLR',
-    //    url: 'https://motor.olrmayorista.com'
+    //{
+    //    operadora: 'Regio',
+    //    url: 'https://b2b.regio.travel/home',
+    //    funct: regio_func
     //},
+    //https://vtoreservaciones.com/Hotel/Hotel.aspx
+    //{
+    //    operadora: 'Check Hotel',
+    //    url: 'https://checkhotel.mx/',
+    //    funct: checkhotel_func
+    //},
+    //{
+    //    operadora: 'Bedsonline',
+    //    url: 'https://app.bedsonline.com/main',
+    //    funct: bedsonline_func
+    //},
+    //{
+    //    operadora: 'Azabache',
+    //    url: 'https://azabache.paquetedinamico.com/login.xhtml?microsite=azabache&keepurl=true&url=%2Fhome',
+    //    funct: azabache_func
+    //},
+    {   
+        operadora: 'OLR',
+        url: 'https://motor.olrmayorista.com/home?tripType=ONLY_HOTEL',
+        funct: olr_func
+    },
     //{
     //    operadora: 'Ruta Maya',
     //    url: 'https://www.rutamayatravel.com/sur4/user/login.html'
